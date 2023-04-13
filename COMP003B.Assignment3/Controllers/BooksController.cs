7dc718575927f68aace34853df01ec890fdbdb5d
MatthewAlvarez5 <alvarezmatthew2002@gmail.com>
2023-04-12 17:40:09 -0700

    Add project files.

---
+++ COMP003B.Assignment3/Controllers/BooksController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using COMP003B.Assignment3.Models;
+using System;
+
+namespace COMP003B.Assignment3.Controllers
+{
+    public class BooksController : Controller
+    {
+        private static List<Book> _books = new List<Book>();
+
+        //Get: Books
+        public IActionResult Index()
+        {
+            return View(_books);
+        }
+        //Get: Books/Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!_books.Any(p => p.ISBN == book.ISBN))
+                {
+                    _books.Add(book);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        //Get: Books/Edit/1
+        [HttpGet]
+        public IActionResult Edit(int? isbn)
+        {
+            if (isbn == null)
+            {
+                return NotFound();
+            }
+            var book = _books.FirstOrDefault(p => p.ISBN == isbn);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+        //Post 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int isbn, Book book)
+        {
+            if (isbn != book.ISBN)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                var existingBook = _books.FirstOrDefault(p => p.ISBN == book.ISBN);
+
+                if (existingBook != null)
+                {
+                    existingBook.Title = book.Title;
+                    existingBook.Genre = book.Genre;
+                    existingBook.Author = book.Author;
+                    existingBook.ReleaseYear = book.ReleaseYear;
+                }
+                return RedirectToAction(nameof(Index));
+
+
+            }
+            return View(book);
+        }
+        //get: Books/Delete/1
+        [HttpGet]
+        public IActionResult Delete(int? isbn)
+        {
+            if (isbn == null)
+            {
+                return NotFound();
+            }
+            var book = _books.FirstOrDefault(p => p.ISBN == isbn);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        //Post: Books/Delete/1
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int isbn)
+        {
+            var book = _books.FirstOrDefault(p => p.ISBN == isbn);
+            if (book != null)
+            {
+                _books.Remove(book);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ COMP003B.Assignment3/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment3.Models
+{
+    public class Book
+    {
+        [Required]
+        [Range(0, 10000000000)]
+        public int ISBN { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Author { get; set; }
+        [Required]
+        [StringLength(1000)]
+        public string Genre { get; set; }
+        [Required]
+        [Range(0, 2023)]
+        public int ReleaseYear { get; set; }
+
+    }
+}
